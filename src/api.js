7dce90f28793c8e2f403c5a809d6b3bb3a95d322
mikael-v <mikael-v@outlook.com>
2024-08-12 21:13:32 +0100

    initilaises project and fetches all spells details

--- src/api.js
+++ src/api.js
@@ -1,9 +1,29 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://www.dnd5eapi.co/api" });
+const api = axios.create({ baseURL: "https://www.dnd5eapi.co" });
 
 export const getAllSpells = () => {
-  return api.get("/spells").then((result) => {
+  return api.get("api/spells/").then((result) => {
+    // console.log(result.data.results);
     return result.data.results;
   });
+};
+
+//spell details from spell url
+export const getSpellDetails = async () => {
+  try {
+    const spells = await getAllSpells();
+
+    // Map over the spells array to fetch details for each spell
+    const spellDetailsPromises = spells.map((spell) => {
+      return api.get(spell.url).then((response) => response.data);
+    });
+
+    // Wait for all promises to resolve
+    const allSpellDetails = await Promise.all(spellDetailsPromises);
+
+    return allSpellDetails;
+  } catch (error) {
+    console.error("Error fetching spell details:", error);
+  }
 };
--- src/components/Spells.jsx
+++ src/components/Spells.jsx
@@ -1,4 +1,4 @@
-import { getAllSpells } from "../api";
+import { getSpellDetails } from "../api";
 import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 
@@ -8,7 +8,7 @@ function Spells() {
 
   useEffect(() => {
     setIsLoading(true);
-    getAllSpells().then((result) => {
+    getSpellDetails().then((result) => {
       setIsLoading(false);
       setSpells(result);
     });
@@ -25,6 +25,26 @@ function Spells() {
           <Card.Body id="card-body">
             <Card.Title>
               <h2>{spell.name}</h2>
+              <h3>
+                Level {spell.level} {spell.school.name}
+              </h3>
+              <p>
+                <b>Casting Time: </b> {spell.casting_time}
+              </p>
+              <p>
+                <b>Range: </b> {spell.range}
+              </p>
+
+              <p>
+                <b>Components: </b> {spell.components.join(", ")}
+              </p>
+              <p>
+                <b>Duration: </b> {spell.duration}
+              </p>
+              <p>{spell.desc}</p>
+              <p>
+                <b>Higher Levels: </b> {spell.higher_level}
+              </p>
             </Card.Title>
           </Card.Body>
         </Card>
