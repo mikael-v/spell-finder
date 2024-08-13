e464e45086f589af0cae4a9c5a7f0f69df2e7376
mikael-v <mikael-v@outlook.com>
2024-08-13 10:25:41 +0100

    creates dropdown option to order spells by level

--- src/api.js
+++ src/api.js
@@ -3,27 +3,42 @@ import axios from "axios";
 const api = axios.create({ baseURL: "https://www.dnd5eapi.co" });
 
 export const getAllSpells = () => {
-  return api.get("api/spells/").then((result) => {
-    // console.log(result.data.results);
+  return api.get("/api/spells").then((result) => {
     return result.data.results;
   });
 };
 
-//spell details from spell url
+// Fetch all spell details
 export const getSpellDetails = async () => {
   try {
     const spells = await getAllSpells();
 
-    // Map over the spells array to fetch details for each spell
     const spellDetailsPromises = spells.map((spell) => {
       return api.get(spell.url).then((response) => response.data);
     });
 
-    // Wait for all promises to resolve
     const allSpellDetails = await Promise.all(spellDetailsPromises);
 
     return allSpellDetails;
   } catch (error) {
     console.error("Error fetching spell details:", error);
+  }
+};
+
+// Fetch all spell details and sort by level
+export const getSpellsOrderedByLevel = async () => {
+  try {
+    const spells = await getAllSpells();
+
+    const spellDetailsPromises = spells.map((spell) => {
+      return api.get(spell.url).then((response) => response.data);
+    });
+
+    const allSpellDetails = await Promise.all(spellDetailsPromises);
+    allSpellDetails.sort((a, b) => a.level - b.level);
+
+    return allSpellDetails;
+  } catch (error) {
+    console.error("Error fetching and ordering spell details:", error);
   }
 };
--- src/components/Spells.jsx
+++ src/components/Spells.jsx
@@ -1,60 +1,87 @@
-import { getSpellDetails } from "../api";
+import { getSpellDetails, getSpellsOrderedByLevel } from "../api";
 import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
+import ReactMarkdown from "react-markdown";
 
 function Spells() {
   const [spells, setSpells] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [spellOrder, setSpellOrder] = useState(0);
 
   useEffect(() => {
     setIsLoading(true);
-    getSpellDetails().then((result) => {
-      setIsLoading(false);
-      setSpells(result);
-    });
-  }, []);
+    if (spellOrder === "Level") {
+      getSpellsOrderedByLevel().then((result) => {
+        setIsLoading(false);
+        setSpells(result);
+      });
+    } else {
+      getSpellDetails().then((result) => {
+        setIsLoading(false);
+        setSpells(result);
+      });
+    }
+  }, [spellOrder]);
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
-  return (
-    <ul className="spells-list">
-      {spells.map((spell) => (
-        <li key={spell.index} className="spell-item">
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <h2>{spell.name}</h2>
-                <p>
-                  {spell.level === 0 ? "Cantrip" : `Level ${spell.level}`}{" "}
-                  {spell.school.name}
-                </p>
-              </Card.Title>
+  const handleSpellChange = (event) => {
+    setSpellOrder(event.target.value);
+  };
 
-              <p>
-                <b>Casting Time: </b> {spell.casting_time}
-              </p>
-              <p>
-                <b>Range: </b> {spell.range}
-              </p>
-              <p>
-                <b>Components: </b> {spell.components.join(", ")}
-              </p>
-              <p>
-                <b>Duration: </b> {spell.duration}
-              </p>
-              <p>{spell.desc}</p>
-              {spell.higher_level && spell.higher_level.length > 0 && (
+  return (
+    <>
+      <select onChange={handleSpellChange}>
+        <option value="">Select Order</option>
+        <option value="Level">Level</option>
+      </select>
+      <ul className="spells-list">
+        {spells.map((spell) => (
+          <li key={spell.index} className="spell-item">
+            <Card>
+              <Card.Body>
+                <Card.Title>
+                  <h2>{spell.name}</h2>
+                  <h3>
+                    <b>
+                      {spell.level === 0 ? "Cantrip" : `Level ${spell.level}`}
+                    </b>{" "}
+                    {spell.school.name}
+                  </h3>
+                </Card.Title>
                 <p>
-                  <b>Higher Levels: </b> {spell.higher_level.join(" ")}
+                  <b>Casting Time: </b> {spell.casting_time}
                 </p>
-              )}
-            </Card.Body>
-          </Card>
-        </li>
-      ))}
-    </ul>
+                <p>
+                  <b>Range: </b> {spell.range}
+                </p>
+                <p>
+                  <b>Components: </b> {spell.components.join(", ")}
+                </p>
+                <p>
+                  <b>Duration: </b> {spell.duration}
+                </p>
+                <ReactMarkdown>
+                  {Array.isArray(spell.desc)
+                    ? spell.desc.join(" ")
+                    : spell.desc}
+                </ReactMarkdown>
+                {spell.higher_level && spell.higher_level.length > 0 && (
+                  <p>
+                    <b>Higher Levels: </b>
+                    <ReactMarkdown>
+                      {spell.higher_level.join(" ")}
+                    </ReactMarkdown>
+                  </p>
+                )}
+              </Card.Body>
+            </Card>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
